dc35bbc7ab9bad5a168ffbafb2969117982dcd1f
luo7023539 <303574922@qq.com>
2016-10-21 13:15:43 +0800

    页面初步完成

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+(function (pro) {
+    /*获取url中的hash及问号传参*/
+    function urlToObj() {
+        var result = {},hash;
+        this.replace(/([^/.=#?]+)=([^/.=#?]+)/, function () {
+            result[arguments[1]] = arguments[2]
+        });
+        hash = this.slice(this.indexOf('#')+1);
+        if(hash){
+            result['hash'] = hash
+        }
+        return result;
+    }
+    pro.urlToObj = urlToObj
+})(String.prototype);
+
+/*自适应改变rem*/
+~function () {
+    var win = document.documentElement.clientWidth || document.body.clientWidth;
+    document.documentElement.style.fontSize = win / 414 * 100 + 'px';
+}();
+
+
+
+
+
